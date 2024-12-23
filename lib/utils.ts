37a244688a84bdc0e947f98dc716df77d55b3198
Wonshik KIM <wonshikkim.kr@gmail.com>
2024-12-24 03:47:22 +0900

    feat: add scene control api

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,7 +8,7 @@ export interface DaliDevice {
     instanceId: number;
   };
   device: {
-    // 장치의 템플릿 식별자: daliDimmableGroup, daliDimmableLight, daliBistableLight
+    // 장치의 템플릿 식별자: daliDimmableGroup, daliDimmableLight, daliBistableLight, daliSceneController
     internalId: string;
   };
   configuration: {
@@ -81,6 +81,24 @@ export async function setDeviceToggle(accessToken: string, serverUrl: string, in
 export async function setDeviceDim(accessToken: string, serverUrl: string, instanceId: string, dim: number) {
   const body = {
     source: 'slider', rowId: 2, command: 'setLevel', params: { slider: dim },
+  };
+  await axios.put(`http://${serverUrl}/api/v1/instance/${instanceId}/panel/action`, body, {
+    headers: { Authorization: `Bearer ${accessToken}` },
+    timeout: 10000, // 10초 타임아웃 설정 (10000밀리초)
+  });
+}
+
+/**
+ * Set the scene of a scene controller.
+ * @param accessToken
+ * @param serverUrl
+ * @param instanceId
+ * @param sceneId The scene ID to set. Should be between 0 and 15. (send GOTO SCENE <sceneId> DALI command)
+ */
+export async function recallScene(accessToken: string, serverUrl: string, instanceId: string, sceneId: number) {
+  const rowId = sceneId % 4;
+  const body = {
+    source: 'button', rowId, command: `setScene${sceneId + 1}`, params: null,
   };
   await axios.put(`http://${serverUrl}/api/v1/instance/${instanceId}/panel/action`, body, {
     headers: { Authorization: `Bearer ${accessToken}` },
